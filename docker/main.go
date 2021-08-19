df2940083aac468d69b4d49d248d855071626c26
Richard Jonas <richard.jonas.76@gmail.com>
2021-08-19 23:28:19 +0200

    Docker client example

---
+++ docker/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, image := range images {
+		fmt.Println("Image:", image.ID, "Size:", image.Size)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		fmt.Println("Images:", container.Image, "with ID:", container.ID)
+	}
+}
